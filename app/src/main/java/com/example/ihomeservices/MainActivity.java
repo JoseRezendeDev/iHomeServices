9e92766b9ac1829a165e474ec43988bdd2254373
JoseRezendeDev <zerezende12345@gmail.com>
2020-07-28 21:29:13 -0300

    Tela de login e de cadastro

---
+++ app/src/main/java/com/example/ihomeservices/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ihomeservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+    private TextView txtCadastrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        txtCadastrar = findViewById(R.id.txtCadastrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ServicesActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        txtCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
