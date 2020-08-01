25fd3f8c9e85b54d30ef37cc31ffc314153145c1
JoseRezendeDev <zerezende12345@gmail.com>
2020-08-01 00:18:35 -0300

    Refatoração da tela de cadastro

--- app/src/main/java/com/example/ihomeservices/RegisterActivity.java
+++ app/src/main/java/com/example/ihomeservices/RegisterActivity.java
@@ -11,20 +11,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity implements RegisterTrabalhadorFragment.OnFragmentInteractionListener {
+public class RegisterActivity extends
+        AppCompatActivity implements
+        RegisterTipoUsuarioFragment.OnFragmentInteractionListener,
+        RegisterTrabalhadorFragment.OnFragmentInteractionListener,
+        RegisterClienteFragment.OnFragmentInteractionListener {
 
     private Button btnCadastrar;
+
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
 
-        RegisterTrabalhadorFragment registerTrabalhadorFragment = new RegisterTrabalhadorFragment();
-        fragmentTransaction.add(R.id.flRegister, registerTrabalhadorFragment);
+        RegisterTipoUsuarioFragment registerTipoUsuarioFragment = new RegisterTipoUsuarioFragment();
+        fragmentTransaction.add(R.id.flRegister, registerTipoUsuarioFragment);
         fragmentTransaction.commit();
 
         btnCadastrar = findViewById(R.id.btnCadastrar);
@@ -40,7 +47,37 @@ public class RegisterActivity extends AppCompatActivity implements RegisterTraba
     }
 
     @Override
+    public void onTipoDeUsuarioSelected(int checkedId) {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        switch (checkedId) {
+            case R.id.rbTrabalhador:
+                RegisterTrabalhadorFragment registerTrabalhadorFragment = new RegisterTrabalhadorFragment();
+                fragmentTransaction.replace(R.id.flRegister, registerTrabalhadorFragment);
+                break;
+            case R.id.rbCliente:
+                RegisterClienteFragment registerClienteFragment = new RegisterClienteFragment();
+                fragmentTransaction.replace(R.id.flRegister, registerClienteFragment);
+                break;
+        }
+        fragmentTransaction.commit();
+        btnCadastrar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
     public void onFragmentInteraction(Uri uri) {
-        System.out.println("Interagiu com fragment");
+        System.out.println("Tipo usuario escolhido");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = fragmentManager.findFragmentById(R.id.flRegister);
+        if (fragment instanceof RegisterTipoUsuarioFragment) {
+            super.onBackPressed();
+        } else {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            RegisterTipoUsuarioFragment registerTipoUsuarioFragment = new RegisterTipoUsuarioFragment();
+            fragmentTransaction.replace(R.id.flRegister, registerTipoUsuarioFragment);
+            fragmentTransaction.commit();
+        }
     }
 }
